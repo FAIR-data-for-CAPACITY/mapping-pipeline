6d47f06898075ac6b1bfc5a4ac1ba8da074e8a4a
Djura Smits <djura.smits@gmail.com>
2021-05-25 15:23:18 +0200

    removed redundand stuff

--- capacity_mapping/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Documentation about capacity_mapping"""
-import logging
-
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Djura Sarina Smits"
-__email__ = "d.smits@esciencecenter.nl"
-__version__ = "0.1.0"
